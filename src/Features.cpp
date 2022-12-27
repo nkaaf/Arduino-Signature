202b6f39dc728c4a17e4d63d202359092523a07d
nkaaf <nkaaf@protonmail.com>
2022-12-27 16:52:42 +0100

    fix format

--- src/Features.cpp
+++ src/Features.cpp
@@ -1,27 +1,27 @@
 /*!
-* @file Features.cpp
-*
-* This file is part of the Signature library. It gives easy access to the
-* signature of AVR microcontrollers. The library contains functions that
-* provides the information of the signature bytes.
-*
-* Copyright (C) 2022  Niklas Kaaf
-*
-* This library is free software; you can redistribute it and/or
-* modify it under the terms of the GNU Lesser General Public
-* License as published by the Free Software Foundation; either
-* version 2.1 of the License, or (at your option) any later version.
-*
-* This library is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-* Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public
-* License along with this library; if not, write to the Free Software
-* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
-* USA
-*/
+ * @file Features.cpp
+ *
+ * This file is part of the Signature library. It gives easy access to the
+ * signature of AVR microcontrollers. The library contains functions that
+ * provides the information of the signature bytes.
+ *
+ * Copyright (C) 2022  Niklas Kaaf
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ */
 
 #include "Features.hpp"
 
